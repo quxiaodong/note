4a8075a7b593fa5b5b4420bb11efa2e6b6989daa
quxiaodong <quxiaodong1990@gmail.com>
2025-04-20 12:26:47 +0800

    :memo:

---
+++ javascript/worker.js
@@ -0,0 +1,38 @@
+function fetchData(item) {
+  item[2] = new Date()
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      item[3] = new Date()
+      resolve(item)
+    }, Math.random() * 2000)
+  })
+}
+
+async function concurrentLimit(array, limit) {
+  const results = []
+  const executors = new Set()
+
+  for (const item of array) {
+    const promise = Promise.resolve().then(() => fetchData(item))
+
+    results.push(promise)
+    executors.add(promise)
+
+    if (executors.size >= limit) {
+      self.postMessage({ type: 'progress', progress: results.length })
+      await Promise.race(executors)
+    }
+
+    promise.finally(() => executors.delete(promise))
+  }
+
+  return Promise.allSettled(results)
+}
+
+self.onmessage = async (event) => {
+  const { array, limit } = event.data
+
+  const results = await concurrentLimit(array, limit)
+
+  self.postMessage({ type: 'result', results })
+}
